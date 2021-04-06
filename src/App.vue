5d78392942a9db6cfa047c20bd0b0539f54bed95
Manuel Sanchez <manuelsanchez128354@correo.itm.edu.co>
2021-04-06 09:28:22 -0500

    learn vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="header"></div>
+    <div id="main-container">
+      <h2>Todos</h2>
+      <Todos v-bind:todoslist="copyTodos" v-on:delete-todo="deleteTodo" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import Search from "@/components/Search";
+import Todos from "@/components/Todos";
+//import TodoAdd from "@/components/TodoAdd";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    //Search,
+    Todos,
+    //TodoAdd,
+  },
+  methods: {
+    deleteTodo(id) {
+      console.log(id);
+    },
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: "Estudiar VUE",
+          completed: false,
+        },
+        {
+          id: 0,
+          title: "Jugar COD mobile",
+          completed: false,
+        },
+        {
+          id: 0,
+          title: "Estudiar JS",
+          completed: true,
+        },
+      ],
+      copyTodos: [],
+    };
+  },
+  created() {
+    this.copyTodos = [...this.todos];
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.5em;
+  padding: 0;
+  margin: 0;
+}
+#main-container {
+  border: solid 1px #ccc;
+  width: 600px;
+  margin: 100px auto;
+}
+#header {
+  background: black;
+  padding: 10px;
+}
+h2 {
+  padding: 0 10px;
 }
 </style>
